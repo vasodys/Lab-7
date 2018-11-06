621fa22a5a717a53aaa22016a070bb88bd85b545
Vasilis Odysseos <vasilis@odysseos.com>
2018-11-06 12:10:55 -0800

    Created repo added files

---
+++ Lab 7/Lab 7/company.cpp
@@ -0,0 +1,100 @@
+// FILE: company.cpp
+// CLASS implemented: company (see company.h for documentation)
+
+
+#include <cassert>
+#include "company.h"
+
+//#define USEDEBUG
+
+#ifdef USEDEBUG
+#define Debug( x ) std::cout << x
+#else
+#define Debug( x )
+#endif
+
+namespace coen79_lab7
+{
+    company::company() {
+        this->company_name = "";
+        this->head_ptr = NULL;
+        this->tail_ptr = NULL;
+    }
+    
+    company::company(const std::string& company_name) {
+        assert(company_name.length() > 0);
+
+        // COMPLETE THE IMPLEMENTATION...
+    }
+    
+    company::company(const company &src) {
+        Debug("Company copy constructor..." << std::endl);
+
+        // COMPLETE THE IMPLEMENTATION...
+    }
+
+    
+    company& company::operator= (const company &src) {
+        Debug("Company assignemnt operator..." << std::endl);
+
+        // COMPLETE THE IMPLEMENTATION...
+    }
+    
+    
+    company::~company() {
+        list_clear(head_ptr);
+    }
+    
+    
+    std::string company::get_name() const {
+        return company_name;
+    }
+    
+    const node *company::get_head() const {
+        return head_ptr;
+    }
+    
+    const node *company::get_tail() const {
+        return tail_ptr;
+    }
+    
+    node* company::get_head() {
+        return head_ptr;
+    }
+    
+    node* company::get_tail() {
+        return tail_ptr;
+    }
+    
+    void company::print_items() {
+        list_print(head_ptr);
+    }
+    
+    
+    bool company::insert(const std::string& product_name, const float& price) {
+        
+        assert(product_name.length() > 0);
+
+        if (list_contains_item(head_ptr, product_name)) {
+            return false;
+        }
+        
+        if (head_ptr == NULL) {
+            // COMPLETE THE IMPLEMENTATION...
+        }
+        else {
+            // COMPLETE THE IMPLEMENTATION...
+        }
+        
+        return true;
+    }
+
+    
+    bool company::erase(const std::string& product_name) {
+        assert(product_name.length() > 0);
+
+        // COMPLETE THE IMPLEMENTATION...
+    }
+    
+    
+}
